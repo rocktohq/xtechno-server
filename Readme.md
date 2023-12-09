254c6e6631d7df939f7dcde612fad6172b4c6d32
rocktohq <itzmonir@gmail.com>
2023-12-09 13:30:54 +0600

    Server Updated

---
+++ Readme.md
@@ -0,0 +1,29 @@
+# xTechno - Server
+
+## Technologies Used:
+- ExpressJS
+- MongoDB
+
+## Setup
+
+### Install Dependencies:
+
+```
+npm install
+```
+
+### Environment Variables
+```javascript
+DB_USER=MongoDB_DB_User
+DB_PASS=MongoDB_DB_Password
+```
+
+### Start the Server:
+
+```
+nodemon index.js
+```
+The server will be run on localhost on port 5000
+```
+http://localhost:5000
+```
--- index.js
+++ index.js
@@ -28,8 +28,6 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
 
     // Collections
     const productCollection = client.db("xTechno").collection("products");
@@ -107,12 +105,9 @@ async function run() {
       res.send(result);
     });
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("You successfully connected to MongoDB!");
+
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+
   }
 }
 run().catch(console.dir);
